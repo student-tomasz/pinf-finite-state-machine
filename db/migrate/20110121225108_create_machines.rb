508b901954dcb89462e686e0daaee4d77b437909
Tomasz Cudziło <tom.cudzilo@gmail.com>
2011-01-22 00:06:56 +0100

    add columns to Machine migration

--- db/migrate/20110121225108_create_machines.rb
+++ db/migrate/20110121225108_create_machines.rb
@@ -1,6 +1,12 @@
 class CreateMachines < ActiveRecord::Migration
   def self.up
     create_table :machines do |t|
+      t.string :name, :unique => true
+      t.string :states_json
+      t.string :alphabet_json
+      t.string :start_state_json
+      t.string :accept_states_json
+      t.text   :transition_json
 
       t.timestamps
     end
