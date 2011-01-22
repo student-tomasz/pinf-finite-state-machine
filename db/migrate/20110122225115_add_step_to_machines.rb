9aeca3125116ce63bee3de381c3631a03d95c9da
Tomasz Cudziło <tom.cudzilo@gmail.com>
2011-01-22 23:55:33 +0100

    add step column to Machine

---
+++ db/migrate/20110122225115_add_step_to_machines.rb
@@ -0,0 +1,12 @@
+class AddStepToMachines < ActiveRecord::Migration
+  def self.up
+    change_table :machines do |t|
+      t.string :step, :default => "initialize"
+    end
+    Machine.update_all :step => "complete"
+  end
+
+  def self.down
+    remove_column :machines, :step
+  end
+end
